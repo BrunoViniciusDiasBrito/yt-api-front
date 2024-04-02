3f0d0b378e443f0f238a5e481c434493131ae99f
Bruno Vinicius Dias de Brito <brunovinicius31121995@gmail.com>
2024-04-02 17:45:19 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App () {
+  return (
+    <div className='w-full min-h-screen bg-gray-900'>
+      <main className='my-10 w-full'>
+        <h1>Clientes</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App
